1e901559556dadfa1f7f53b1a71f2c485d5ccb09
JDaniel <daniel@JDStart.onmicrosoft.com>
2024-05-12 19:16:06 +0100

    Add buttons card

---
+++ portfolio/src/components/groupButtons/ButtonsCard.vue
@@ -0,0 +1,20 @@
+<script setup>
+    import ButtonCardFigma from '../buttons/ButtonCardFigma.vue';
+    import ButtonCardWhiteBoard from '../buttons/ButtonCardWhiteBoard.vue';
+    import ButtonCardGithub from '../buttons/ButtonCardGithub.vue'
+
+</script>
+
+<template>
+    <div>
+        <ButtonCardFigma></buttonCardFigma>
+        <ButtonCardWhiteboard></buttonCardWhiteboard>
+        <ButtonCardGithub></buttonCardGithub>
+    </div>
+</template>
+
+<style scoped>
+    .buttonGroup {
+        display: flex;
+    }
+</style>
