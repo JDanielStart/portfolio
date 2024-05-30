73a28d88ad88d91c97e8374ca5fb3011dcce711a
JDaniel <daniel@JDStart.onmicrosoft.com>
2024-05-30 18:14:10 +0100

    Create icon component

---
+++ portfolio/src/components/icons/Icon.vue
@@ -0,0 +1,34 @@
+<template>
+    <svg
+      :width="width"
+      :height="height"
+      :viewBox="viewBox"
+      xmlns="http://www.w3.org/2000/svg"
+      role="presentation"
+    >
+      <g :fill="iconColor">
+        <slot/>
+      </g>
+    </svg>
+  </template>
+  
+  <script setup>
+    import { useIconStore } from '../../stores/IconStore.js';
+    import { inject } from 'vue';
+    import { storeToRefs } from 'pinia';
+
+    //Stores
+    const store = inject('store') || useIconStore();
+
+    //States
+    const { iconColor, width, height, viewBox,} = storeToRefs(store);
+
+    //Actions
+
+    //Change styles
+
+  </script>
+
+  <style scoped>
+
+  </style>
