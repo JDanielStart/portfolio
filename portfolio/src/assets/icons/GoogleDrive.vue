317edeb2f3f6524065f986cae2322cf21e857e75
JDaniel <daniel@JDStart.onmicrosoft.com>
2024-06-02 22:19:28 +0100

    Created svg path components

---
+++ portfolio/src/assets/icons/GoogleDrive.vue
@@ -0,0 +1,10 @@
+<template>
+    <g clip-path="url(#clip0_442_15627)">
+        <path d="M13.7417 12.3008L7.35107 1.25H13.6479L20.0386 12.3008H13.7417ZM8.37061 13.2227L5.22217 18.75H17.3511L20.4995 13.2227H8.37061ZM6.51904 2.63281L0.499512 13.2227L3.64795 18.75L9.75732 8.15625L6.51904 2.63281Z"/>
+    </g>
+    <defs>
+        <clipPath id="clip0_442_15627">
+            <rect width="20" height="20" fill="white" transform="translate(0.499512)"/>
+        </clipPath>
+    </defs>
+</template>
