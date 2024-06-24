67831478957cf62b479dc63a90618d8a9d4166e9
JDaniel <daniel@JDStart.onmicrosoft.com>
2024-06-23 20:14:57 -0400

    Icon GoogleDrive

--- portfolio/src/assets/icons/GoogleDrive.vue
+++ portfolio/src/assets/icons/GoogleDrive.vue
@@ -1,6 +1,6 @@
 <template>
     <g clip-path="url(#clip0_442_15627)">
-        <path d="M13.7417 12.3008L7.35107 1.25H13.6479L20.0386 12.3008H13.7417ZM8.37061 13.2227L5.22217 18.75H17.3511L20.4995 13.2227H8.37061ZM6.51904 2.63281L0.499512 13.2227L3.64795 18.75L9.75732 8.15625L6.51904 2.63281Z"/>
+        <path :fill="color" d="M13.7417 12.3008L7.35107 1.25H13.6479L20.0386 12.3008H13.7417ZM8.37061 13.2227L5.22217 18.75H17.3511L20.4995 13.2227H8.37061ZM6.51904 2.63281L0.499512 13.2227L3.64795 18.75L9.75732 8.15625L6.51904 2.63281Z"/>
     </g>
     <defs>
         <clipPath id="clip0_442_15627">
@@ -8,3 +8,16 @@
         </clipPath>
     </defs>
 </template>
+
+<script setup>
+  import { defineProps, computed } from 'vue';
+
+  //Props
+  const props = defineProps({
+        color: {
+            type: String
+        }
+    });
+    
+    const color = computed(() => props.color);
+</script>
