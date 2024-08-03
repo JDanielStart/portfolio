6be38fdd6c8310570002d0325214280a0b77e075
JDaniel <daniel@JDStart.onmicrosoft.com>
2024-08-03 17:23:03 -0400

    chore: Update detection scheme color of web browser

--- portfolio/src/components/ctas/CtaEnd.vue
+++ portfolio/src/components/ctas/CtaEnd.vue
@@ -184,4 +184,30 @@
         font-size: var(--font-size-21);
         line-height: 2.4rem;
     }
+
+    /* Animation */
+    .vibrate{
+        animation:vibrate 1.5s linear both
+    }
+    
+    @keyframes vibrate{
+        0%{
+            transform:translate(0)
+        }
+        20%{
+            transform:translate(-2px,2px)
+        }
+        40%{
+            transform:translate(-2px,-2px)
+        }
+        60%{
+            transform:translate(2px,2px)
+        }
+        80%{
+            transform:translate(2px,-2px)
+        }
+        100%{
+            transform:translate(0)
+        }
+    }
 </style>
