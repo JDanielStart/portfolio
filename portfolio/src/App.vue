6341be004adfe573060842741f54d493f55f3a4c
JDaniel <daniel@JDStart.onmicrosoft.com>
2024-08-04 04:53:36 -0400

    chore: Add animation and accessibility improvements to App.vue

--- portfolio/src/App.vue
+++ portfolio/src/App.vue
@@ -150,6 +150,7 @@
     const classes= {};
 
     classes['buttonLanguage'] = true;
+    classes['animation-button-language'] = true;
 
     return classes;
   });
@@ -222,6 +223,7 @@
       top: 20rem;
       right: 0.5rem;
       z-index: 1;
+      opacity: 0;
   }
 
   .hero {
@@ -267,6 +269,47 @@
       justify-content: center;
       width:100%;
       z-index: 1;
+    }
+  }
+
+  /* Animation */
+  .animation-button-language {
+    opacity: 0;
+    animation:
+      invisibility 1s ease-in-out 2.5s forwards,
+      jello-horizontal 1s ease-in-out 2.5s forwards;
+  }
+
+  @keyframes invisibility {
+    0% {
+        opacity: 0;
+    }
+    100% {
+        opacity: 1;
+    }
+  }
+
+  @keyframes jello-horizontal {
+    0% {
+        transform: scale3d(1, 1, 1);
+    }
+    30% {
+        transform: scale3d(1.25, 0.75, 1);
+    }
+    40% {
+        transform: scale3d(0.75, 1.25, 1);
+    }
+    50% {
+        transform: scale3d(1.15, 0.85, 1);
+    }
+    65% {
+        transform: scale3d(0.95, 1.05, 1);
+    }
+    75% {
+        transform: scale3d(1.05, 0.95, 1);
+    }
+    100% {
+        transform: scale3d(1, 1, 1);
     }
   }
   
